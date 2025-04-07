09b9ae881cec687906eefde77037fa4b00ba485d
pd12patil <pratikdighe3005@gmail.com>
2025-04-07 09:48:36 +0530

    correction for hosting

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,42 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client';
+// import { BrowserRouter, Route, Routes } from "react-router";
+// import './index.css'
+
+// import Home from './views/Home';
+// import Add from './views/Add';
+// import Edit from './views/Edit';
+
+// createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//    <Routes>
+//     <Route path="/" element={<Home />}/>
+//     <Route path="/add" element={<Add />}/>
+//     <Route path="/edit/:userId" element={<Edit />}/>
+//    </Routes>
+//   </BrowserRouter>
+  
+// )
+
+
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from "react-router";
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  // Correct import from 'react-router-dom'
+import './index.css';
 
 import Home from './views/Home';
 import Add from './views/Add';
 import Edit from './views/Edit';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="/add" element={<Add />}/>
-    <Route path="/edit/:userId" element={<Edit />}/>
-   </Routes>
-  </BrowserRouter>
-  
-)
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/edit/:userId" element={<Edit />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
+
